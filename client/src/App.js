4efd68cbeef0b7ff97b08e5fffff22df81dd7336
Luiz Felipe <luizfelper@gmail.com>
2022-04-08 10:57:11 -0300

    Retornando os valores da API na tela usando .map

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
-import React, { useState, Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import './App.css';
 
 function App() {
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState([{resposta: ''}, {resposta02: ''}]);
 
-  function componentDidMount() {
+  useEffect(() => {
     callApi()
-    .then(res => setResponse({ response: res.express}))
-    .catch(err => console.log(err));
-  }
-  componentDidMount();
+    .then(res => setResponse([{ resposta: res.name}, {resposta02: res.express}]))
+    .catch(err => console.log(err));},
+    [])
 
   async function callApi() {
     const response = await fetch('/api/mensagem');
@@ -18,10 +17,19 @@ function App() {
     return body;
   }
 
+  console.log(response);
+
   return (
     <div className="App">
       <header className="App-header">
-        {response.response}
+        {response.map((response) => { 
+          return (
+            <>
+            <h1>{response.resposta}</h1>
+            <h1>{response.resposta02}</h1>
+          </>
+          );
+        })}
       </header>
     </div>
   );
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/mensagem', (req, res) => {
-    res.send({ express: 'Esse é um dado da API interna' });
+    res.send({ name: 'Servidor:', express: 'Esse é um dado da API interna' });
 });
 
 app.listen(port, () => console.log(`Escutando na porta ${port}`));
